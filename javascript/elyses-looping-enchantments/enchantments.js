f8c022b8e28b0d0b32bfbc2aac54a5da15ffa513
Callum D E Smith <cdes@cdesmith.co.uk>
2024-05-11 22:21:11 +0100

    Solved Elyses Looping Enchantments

--- javascript/elyses-looping-enchantments/enchantments.js
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,12 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0;
+  stack.forEach((current) => {
+    count += current === card ? 1 : 0
+  });
+
+  return count;
 }
 
 /**
@@ -20,5 +25,12 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+  const findType = type ? 0 : 1;
+  let count = 0;
+  for(const card of stack) {
+    if(card % 2 === findType) {
+      count +=1;
+    }
+  }
+  return count;
 }
